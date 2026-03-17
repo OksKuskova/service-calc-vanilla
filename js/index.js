164ded96291f7b5891da764162169544fda20c2f
OksKuskova <oskanchik.kuskova@gmail.com>
2026-03-17 18:17:21 +0300

    feat: Подключит renderSelect в index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-import { getData } from "./api/api.js"
+import { getData } from "./api/api.js";
+import { renderSelect } from "./select.js";
 
 async function initCalc() {
 	try {
 		const data = await getData();
-		console.log(data);
+		renderSelect(data);
 
 	} catch (error) {
 		console.log(error);
